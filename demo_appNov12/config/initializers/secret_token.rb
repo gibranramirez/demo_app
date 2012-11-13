c81b5642b3fb3fcec795d7fc8b5c993351d26cb4
Gibran_Ramirez <gibran.abarca@gmail.com>
2012-11-12 20:38:30 -0500

    Initial commit

---
+++ demo_appNov12/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppnov12::Application.config.secret_token = '80b6bfd7a4d4a74634d6d2e2b989f8e1130b8743c6e8115c38cbb1538ae21cd0699bd88e65fdd1534f7286ec4522cbe528801796a644afbc984d3f9cd29f8c9d'
